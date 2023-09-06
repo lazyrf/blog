65f5ec05865522b1601e98fa196f1d73226d9bc1
Walker <lazyrf.tw@gmail.com>
2023-09-06 20:21:00 +0800

    Create templates for views and add URL patterns

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from blog.models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    # Retrieve the object that matches the given parameters or and Http404 exceptioon if no object is found
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No post found.")
+
+    return render(request, 'blog/post/detail.html', {'post': post})
