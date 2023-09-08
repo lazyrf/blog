e39c5c36cd55e615fb241c7731a6fe85cbdef358
Walker <lazyrf.tw@gmail.com>
2023-09-08 23:40:13 +0800

    Add tag feature

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,12 +4,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 from blog.models import Post, Comment
 from blog.forms import EmailPostForm, CommentForm
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -22,7 +27,7 @@ def post_list(request):
     except EmptyPage:
         # If page_nubmer is out of range
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
